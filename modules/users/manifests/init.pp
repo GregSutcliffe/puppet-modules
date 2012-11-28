60a8ba7d690d948e0b773492359f4b36f2c649bb
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2012-11-28 23:38:28 +0000

    Add my ssh key to my user

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -11,4 +11,17 @@ class users {
     uid        => 2000,
   }
 
+  file { '/home/greg/.ssh':
+    ensure => directory,
+    mode   => 0700,
+    owner  => 'greg',
+  }
+
+  file { '/home/greg/.ssh/authorized_keys':
+    ensure  => present,
+    mode    => 0600,
+    owner   => 'greg',
+    content => template('users/greg.sshkey.pub')
+  }
+
 }
