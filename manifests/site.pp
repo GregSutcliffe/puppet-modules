350c66da78142e151de0e4755da637a0690cf5c0
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2012-11-28 20:33:58 +0000

    Symlink systemctl on Arch

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,12 @@
 node default {
   case $operatingsystem {
-    'Archlinux': { Service { provider => "systemd" } }
+    'Archlinux': { 
+      Service { provider => "systemd" }
+      file { '/bin/systemctl':
+        ensure => link,
+        target => '/usr/bin/systemctl',
+      }
+    }
     default:     { }
   }
 }
