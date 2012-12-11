99b46aa432c7cc13f37dcba3cf68e716976fb946
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2012-12-11 23:20:34 +0000

    Ensure pacman is kept up to date

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,25 @@
 node default {
   case $operatingsystem {
-    'Archlinux': { 
+    'Archlinux': {
+
+      # Always use systemd, init is broken on Arch
       Service { provider => "systemd" }
+
+      # System is not in the same place as Puppet thinks
       file { '/bin/systemctl':
         ensure => link,
         target => '/usr/bin/systemctl',
       }
+
+      # Ensure repos are up-to-date
+      file { '/etc/cron.hourly/pacman-repo-update':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => 0744,
+        content => "#!/bin/bash\nexec ionice -c 2 -n 7 nice -n 19 pacman -Sy\n"
+      }
+
     }
     default:     { }
   }
